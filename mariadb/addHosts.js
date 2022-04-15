01691770ceb15f8712c996aa70ea4c4e0b03607a
Arayaphong A. Traisopon <arme.tr@gmail.com>
2022-04-15 16:18:13 +0700

    create table hosts if not exist

--- mariadb/addHosts.js
+++ mariadb/addHosts.js
@@ -12,12 +12,22 @@ const addHosts = (hosts, done) => {
         const dbHosts = await conn.query("SELECT host_id FROM hosts;");
         const dbHodtIds = dbHosts.map(host => host.host_id);
         hosts = hosts.filter(host => !dbHodtIds.includes(parseInt(host.hostid)));
-        if(hosts.length <=0) return done(null, "Nothing new hosts")
+        if (hosts.length <= 0) return done(null, "Nothing new hosts")
 
         const values = hosts.map(host => "(" + host.hostid + ", '" + host.name + "', NOW())");
         const sql = "INSERT INTO hosts VALUES" + values.join(",") + ";"
         const res = await conn.query(sql);
         done(null, res);
+    }
+
+    async function createTable(conn) {
+        const sql = "CREATE TABLE `hosts` (\
+            `host_id` int(11) NOT NULL,\
+            `host_name` varchar(255) DEFAULT NULL,\
+            `create_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),\
+            PRIMARY KEY (`host_id`)\
+          );"
+        return await conn.query(sql);
     }
     async function asyncFetch() {
         let conn;
@@ -25,12 +35,8 @@ const addHosts = (hosts, done) => {
             conn = await pool.getConnection();
             const rows = await conn.query("SHOW TABLES LIKE 'hosts';");
             const existing = (rows.length > 0);
-            if (existing) await sqlInsert(conn, done)
-            else {
-                done(null, rows); //[ {val: 1}, meta: ... ]
-            }
-
-
+            if (!existing) await createTable(conn, done);
+            await sqlInsert(conn, done);
         } catch (err) {
             done(err);
         } finally {
