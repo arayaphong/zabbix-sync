447a9aade29db246df70462b0fc59d0acd4bbb00
arayaphong <arme.tr@gmail.com>
2022-04-17 14:21:12 +0700

    check finished process

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const sync = synchronizer();
 sync.hosts((err, result) => {
     if (err) console.error(err);
     else {
+        var doneCount = 0;
         const updater = result.updater;
         const hosts = result.hosts;
         hosts.forEach(host => {
             const hostId = host.hostid;
             const done = (err, result) => {
+                doneCount++;
                 if (err) console.error(err);
-                else console.log(result)
+                else console.log(result);
+
+                if (doneCount >= hosts.length * Object.keys(updater).length) process.exit();
             }
             const update = (res) => console.log(res);
             updater.cpu(hostId, update, done);
